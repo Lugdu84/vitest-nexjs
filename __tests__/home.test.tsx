ef5682556352e73efcfb7bf57773dbeb511199be
David Grammatico <david.lugdunum@gmail.com>
2023-10-21 16:02:16 +0200

    update config

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -1,13 +1,14 @@
 
 
 import { render, screen } from '@testing-library/react';
+import "@testing-library/jest-dom";
 import { describe, it, expect} from "vitest"
 import Home from '../app/page';
 
 describe('Home page', () => {
   it('should render the heading', () => {
     render(<Home />);
-    const heading = screen.getByText('Docs');
-    expect(heading).toBeDefined();
+    const heading = screen.getByText(/docs/i);
+    expect(heading).toBeInTheDocument();
   });
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -9,4 +9,5 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
   },
+
 })
