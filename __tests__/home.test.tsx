ba304359414e67a2ac88fafa0e7ba500d0a1234a
David Grammatico <david.lugdunum@gmail.com>
2023-10-21 22:38:39 +0200

    config eslint

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -1,14 +1,12 @@
-
-
-import { render, screen } from '@testing-library/react';
-import  "@testing-library/jest-dom/vitest";
-import { describe, it, expect } from "vitest"
-import Home from '../app/page';
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/vitest'
+import { describe, it, expect } from 'vitest'
+import Home from '../app/page'
 
 describe('Home page', () => {
   it('should render the heading', () => {
-    render(<Home />);
-    const button = screen.getByRole("link", { name: "Home Page" });
-    expect(button).toBeInTheDocument();
-  });
-});
+    render(<Home />)
+    const button = screen.getByRole('link', { name: 'Home Page' })
+    expect(button).toBeInTheDocument()
+  })
+})
--- __tests__/navbar.test.tsx
+++ __tests__/navbar.test.tsx
@@ -1,28 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import  "@testing-library/jest-dom/vitest";
-import { describe, it, expect } from "vitest"
-import Navbar from '../components/navbar';
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/vitest'
+import { describe, it, expect } from 'vitest'
+import Navbar from '../components/navbar'
 
 const user = {
-    name: "John Doe"
+  name: 'John Doe',
 }
 
 describe('Navbar', () => {
-    it('should render the heading Inscription and Connexion if no User', () => {
-        render(<Navbar />);
-        const signupLink = screen.getByRole("link", { name: "Inscription" });
-        const signinLink = screen.getByRole("link", { name: "Connexion" });
-        expect(signupLink).toBeInTheDocument();
-        expect(signinLink).toBeInTheDocument();
-    })
-});
+  it('should render the heading Inscription and Connexion if no User', () => {
+    render(<Navbar />)
+    const signupLink = screen.getByRole('link', { name: 'Inscription' })
+    const signinLink = screen.getByRole('link', { name: 'Connexion' })
+    expect(signupLink).toBeInTheDocument()
+    expect(signinLink).toBeInTheDocument()
+  })
+})
 
 describe('Navbar', () => {
-    it('should render the heading Profil and Deconnexion if user', () => {
-        render(<Navbar user={user} />);
-        const profilLink = screen.getByRole("link", { name: "Profil" });
-        const logoutLink = screen.getByRole("link", { name: "Deconnexion" });
-        expect(profilLink).toBeInTheDocument();
-        expect(logoutLink).toBeInTheDocument();
-    })
-});
+  it('should render the heading Profil and Deconnexion if user', () => {
+    render(<Navbar user={user} />)
+    const profilLink = screen.getByRole('link', { name: 'Profil' })
+    const logoutLink = screen.getByRole('link', { name: 'Deconnexion' })
+    expect(profilLink).toBeInTheDocument()
+    expect(logoutLink).toBeInTheDocument()
+  })
+})
