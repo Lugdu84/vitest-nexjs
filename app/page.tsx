aa0e1e6b380e1f0f2e9a7a814960e85ca924beb7
David Grammatico <david.lugdunum@gmail.com>
2023-11-17 17:18:06 +0100

    Refactor code and fix imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
-import Counter from '@/components/counter'
-import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
     <main className="flex items-center gap-3 p-24">
-      <Link href={"/"}>
-        <Button variant={"ghost"}>Home Page</Button>
+      <Link href="/">
+        <Button variant="ghost">Home Page</Button>
       </Link>
-      <Link href={"/counter"}>
-        <Button variant={"ghost"}>Counter Page</Button>
+      <Link href="/counter">
+        <Button variant="ghost">Counter Page</Button>
       </Link>
     </main>
   )
